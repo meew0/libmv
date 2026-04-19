10d85dc136b30de8bc74133fafec246a6eb26346
meew0 <meew0@mz.sb>
2026-04-19 21:00:38 +0200

    Keep the revision file static rather than regenerating it at build time

--- src/CMake/LibmvVersion.cmake
+++ src/CMake/LibmvVersion.cmake
@@ -24,7 +24,7 @@ SET(LIBMV_VERSION_PATCH 0) #or the svn revision
 
 # The standard GNU version numbering scheme is major.minor.revision
 IF (EXISTS ${LIBMV_SOURCE_DIR}/.svn)
-  # Probably a SVN workspace, determine revision level 
+  # Probably a SVN workspace, determine revision level
   FIND_PACKAGE(Subversion)
   IF(Subversion_FOUND)
     Subversion_WC_INFO(${LIBMV_SOURCE_DIR} ER)
@@ -41,5 +41,3 @@ SET(LIBMV_VERSION "${LIBMV_VERSION_MAJOR}.${LIBMV_VERSION_MINOR}.${LIBMV_VERSION
 # SET(LIBMV_VERSION "${LIBMV_VERSION_MAJOR}.${LIBMV_VERSION_MINOR}-b")
 # Release candidate version [+number]
 # SET(LIBMV_VERSION "${LIBMV_VERSION_MAJOR}.${LIBMV_VERSION_MINOR}-rc")
-
-CONFIGURE_FILE(${CMAKE_CURRENT_SOURCE_DIR}/CMake/revision.h.in ${CMAKE_CURRENT_SOURCE_DIR}/libmv/tools/revision.h)
